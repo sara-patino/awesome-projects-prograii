f79be17eec5ae3b07d58c746e6b320397fdfedff
sara-patino <sara.patino@alumnos.uneatlantico.com>
2023-06-09 20:47:50 +0200

    feature: ServicePayment classes

---
+++ src/classes/transaction/ServicePayment/Deposit.java
@@ -0,0 +1,29 @@
+package classes.transactions;
+
+import classes.Account;
+import classes.Entity;
+import enums.ETransactionTypes;
+import classes.transactions.*;
+
+public class Deposit extends Transaction {
+	
+	public Deposit(double amount, Account inAccount, Entity entity, ETransactionTypes transactionType) {
+		//Llamando al otro constructor
+		super(amount, inAccount, entity, transactionType);
+		this.description = "Dep�sito de: �" + this.amount;
+		
+	}
+	
+	@Override
+	public boolean createTransaction() {
+		try {
+			double newAvalibleMoney = this.inAccount.getAvalibleMoney()+this.amount;
+			this.inAccount.setAvalibleMoney(newAvalibleMoney);
+			this.inAccount.addTransaction(this);
+			this.inAccount.getEntity().addTransaction(this);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+}
